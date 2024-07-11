4504caf0b0a3da06baf4e359e1ad9cd30826d049
jonmail <jonmail@163.com>
2024-07-11 22:42:11 +0800

    Update build-iso-installer-main.sh

--- just_scripts/build-iso-installer-main.sh
+++ just_scripts/build-iso-installer-main.sh
@@ -103,7 +103,7 @@ if [[ ! -f ${project_root}/${flatpak_dir_shortname}/flatpaks_with_deps ]]; then
         -e FLATPAK_TRIGGERSDIR=/flatpak/triggers \
         --volume "${FLATPAK_REFS_DIR}":/output \
         --volume "${TEMP_FLATPAK_INSTALL_DIR}":/temp_flatpak_install_dir \
-        "ghcr.io/ublue-os/${base_image}-main:${version}" /temp_flatpak_install_dir/script.sh
+        "ghcr.io/jonmail/${base_image}-main:${version}" /temp_flatpak_install_dir/script.sh
 fi
 
 # Remove Temp Directory
